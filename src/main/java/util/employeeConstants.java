c7cd7e5efe10422de00a8d84de618ce7da897369
vlado <vladoe12@hotmail.com>
2016-04-19 11:33:28 +0200

    convert date fix

--- src/main/java/util/employeeConstants.java
+++ src/main/java/util/employeeConstants.java
@@ -3,7 +3,7 @@ package util;
 /**
  * Created by Jakub on 19.04.2016.
  */
-public class employeeConstants {
+public class EmployeeConstants {
     public final static String DEALERS = "dealers";
     public final static String CLEANINGMANS = "cleaning-mans";
     public final static String WAREHOUSEMANS = "warehousemans";
