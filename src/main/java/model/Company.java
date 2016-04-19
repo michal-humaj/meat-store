e88a8614ccf2a98bdce0d1345030cf12c8449f1e
Jakub Tutko <jakub.tutko@gmail.com>
2016-04-19 10:21:15 +0200

    company Model

--- src/main/java/model/Company.java
+++ src/main/java/model/Company.java
@@ -10,7 +10,48 @@ import java.util.HashMap;
  */
 public class Company {
 
+    private String name;
+
+    @SerializedName("founding-date")
+    private DateTime foundingDate;
+
+    private Employee director;
+
+    private HashMap employees;
+
     private Warehouse warehouse;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DateTime getFoundingDate() {
+        return foundingDate;
+    }
+
+    public void setFoundingDate(DateTime foundingDate) {
+        this.foundingDate = foundingDate;
+    }
+
+    public Employee getDirector() {
+        return director;
+    }
+
+    public void setDirector(Employee director) {
+        this.director = director;
+    }
+
+    public HashMap getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(HashMap employees) {
+        this.employees = employees;
+    }
 
     public Warehouse getWarehouse() {
         return warehouse;
@@ -19,13 +60,4 @@ public class Company {
     public void setWarehouse(Warehouse warehouse) {
         this.warehouse = warehouse;
     }
-
-    private String name;
-
-    @SerializedName("founding-date")
-    private DateTime foundingDate;
-
-    private E
-
-    private HashMap employees;
 }
