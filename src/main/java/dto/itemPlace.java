b980ac7c678c58d2d381c068683e29507523bd6c
vlado <vladoe12@hotmail.com>
2016-04-19 10:54:09 +0200

    fix

---
+++ src/main/java/dto/itemPlace.java
@@ -0,0 +1,34 @@
+package dto;
+
+import com.google.gson.annotations.SerializedName;
+import org.joda.time.DateTime;
+
+/**
+ * Created by vlado on 19.04.2016.
+ */
+public class ItemPlace {
+
+    @SerializedName("box-number")
+    private int boxNumber;
+    @SerializedName("shelf-number")
+    private String shelfNumber;
+    private int count;
+    @SerializedName("date-of-expiration")
+    private DateTime dateOfExpiration;
+
+    public int getBoxNumber() {
+        return boxNumber;
+    }
+
+    public String getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public DateTime getDateOfExpiration() {
+        return dateOfExpiration;
+    }
+}
