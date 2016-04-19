ddfc665c8465b4d9b9189d84c918d41b106cfaa6
Jan Mjartan <mjartan.jan@gmail.com>
2016-04-19 11:42:02 +0200

    Meat location listing

--- src/main/java/dto/output/ItemPlace.java
+++ src/main/java/dto/output/ItemPlace.java
@@ -1,7 +1,9 @@
 package dto.output;
 
 import com.google.gson.annotations.SerializedName;
-import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vlado on 19.04.2016.
@@ -14,7 +16,7 @@ public class ItemPlace {
     private String shelfNumber;
     private int count;
     @SerializedName("date-of-expiration")
-    private DateTime dateOfExpiration;
+    private String dateOfExpiration;
 
     public int getBoxNumber() {
         return boxNumber;
@@ -28,7 +30,36 @@ public class ItemPlace {
         return count;
     }
 
-    public DateTime getDateOfExpiration() {
+    public String getDateOfExpiration() {
         return dateOfExpiration;
+    }
+
+    public void setBoxNumber(int boxNumber) {
+        this.boxNumber = boxNumber;
+    }
+
+    public void setShelfNumber(String shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setDateOfExpiration(String dateOfExpiration) {
+        this.dateOfExpiration = dateOfExpiration;
+    }
+
+    public static class ItemPlaceList {
+        @SerializedName("item-place")
+        private List<ItemPlace> itemPlaceList = new ArrayList<>();
+
+        public List<ItemPlace> getItemPlaceList() {
+            return itemPlaceList;
+        }
+
+        public void setItemPlaceList(List<ItemPlace> itemPlaceList) {
+            this.itemPlaceList = itemPlaceList;
+        }
     }
 }
--- src/main/java/impl/WarehouseManagerServiceImpl.java
+++ src/main/java/impl/WarehouseManagerServiceImpl.java
@@ -1,15 +1,52 @@
 package impl;
 
 import api.WarehouseManageService;
+import com.google.gson.Gson;
+import dto.input.ItemPlaceInput;
+import dto.output.ItemPlace;
+import model.CoolingBox;
+import model.Meat;
+import model.Shelf;
+import model.Warehouse;
+import org.joda.time.DateTime;
+import util.DateConverter;
+
 
 /**
  * Created by Rex on 19.4.2016.
  */
 public class WarehouseManagerServiceImpl implements WarehouseManageService {
 
+    public static final Gson GSON = new Gson();
+
     @Override
     public String getLocationOfItemInWarehouse(String inputJson) {
-        return null;
+        ItemPlaceInput itemPlaceInput = GSON.fromJson(inputJson, ItemPlaceInput.class);
+
+        Warehouse warehouse = CompanyProvider.getInstance().getAppData().getWarehouse();
+
+        ItemPlace.ItemPlaceList result = new ItemPlace.ItemPlaceList();
+
+        for (CoolingBox coolingBox : warehouse.getCoolingBoxes()) {
+            for (Shelf shelf : coolingBox.getShelves()) {
+                for (Meat meat : shelf.getMeat()) {
+                    if(meat.getMeatType().equals(itemPlaceInput.getMeatType())) {
+                        ItemPlace itemPlace = new ItemPlace();
+                        itemPlace.setBoxNumber(coolingBox.getNumber());
+                        itemPlace.setCount(meat.getCount());
+
+                        DateTime slaughterDate = DateConverter.toDateTimeDots(meat.getDate());
+                        DateTime expiryDate = slaughterDate.plusDays(meat.getMeatType().getFreshDays());
+
+                        itemPlace.setDateOfExpiration(DateConverter.toStringDots(expiryDate));
+                        itemPlace.setShelfNumber(shelf.getNumber());
+                        result.getItemPlaceList().add(itemPlace);
+                    }
+                }
+            }
+        }
+
+        return GSON.toJson(result);
     }
 
     @Override
