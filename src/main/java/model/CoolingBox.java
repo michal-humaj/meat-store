3fba4207c621a50182abdec2c4651a5139688365
Michal Humaj <michal.humaj@opoint.com>
2016-04-19 10:01:28 +0200

    Add models Shelf and Cooling box

---
+++ src/main/java/model/CoolingBox.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mihu on 19.4.16.
+ */
+public class CoolingBox {
+
+    private int number;
+    private List<Shelf> shelves = new ArrayList<Shelf>();
+    private BoxType type;
+
+
+
+}
