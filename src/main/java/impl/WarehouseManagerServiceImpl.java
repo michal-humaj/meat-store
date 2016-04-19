b472f72e9411157828d8d04d59ac1613b9b2d1fb
Jakub Tutko <jakub.tutko@gmail.com>
2016-04-19 12:56:25 +0200

    final push

--- src/main/java/impl/WarehouseManagerServiceImpl.java
+++ src/main/java/impl/WarehouseManagerServiceImpl.java
@@ -80,6 +80,7 @@ public class WarehouseManagerServiceImpl implements WarehouseManageService {
         }
 
         Collections.sort(meats, new MeatDateComparator());
+        
 
         return null;
     }
