c3aa548120f54d5ef22dbac421c5194d9a3edce1
Jan Mjartan <mjartan.jan@gmail.com>
2016-04-19 11:13:39 +0200

    ItemPlaceInput

--- src/main/java/dto/input/ItemPlaceInput.java
+++ src/main/java/dto/input/ItemPlaceInput.java
@@ -1,7 +1,32 @@
 package dto.input;
 
+import com.google.gson.annotations.SerializedName;
+import model.BoxType;
+import model.MeatType;
+
 /**
  * Created by Rex on 19.4.2016.
  */
 public class ItemPlaceInput {
+    @SerializedName("type")
+    private MeatType meatType;
+
+    @SerializedName("cooling-type")
+    private BoxType coolingType;
+
+    public MeatType getMeatType() {
+        return meatType;
+    }
+
+    public void setMeatType(MeatType meatType) {
+        this.meatType = meatType;
+    }
+
+    public BoxType getCoolingType() {
+        return coolingType;
+    }
+
+    public void setCoolingType(BoxType coolingType) {
+        this.coolingType = coolingType;
+    }
 }
