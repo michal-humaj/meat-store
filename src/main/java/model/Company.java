f2d902f7b791b90ebdb30cab8ab2a242b01bb507
Jakub Tutko <jakub.tutko@gmail.com>
2016-04-19 10:20:59 +0200

    company model created

--- src/main/java/model/Company.java
+++ src/main/java/model/Company.java
@@ -1,5 +1,10 @@
 package model;
 
+import com.google.gson.annotations.SerializedName;
+import org.joda.time.DateTime;
+
+import java.util.HashMap;
+
 /**
  * Created by Jakub on 19.04.2016.
  */
@@ -14,4 +19,13 @@ public class Company {
     public void setWarehouse(Warehouse warehouse) {
         this.warehouse = warehouse;
     }
+
+    private String name;
+
+    @SerializedName("founding-date")
+    private DateTime foundingDate;
+
+    private E
+
+    private HashMap employees;
 }
